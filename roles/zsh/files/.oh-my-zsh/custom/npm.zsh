2cf48f4571bb0334b291e55c1e677c591deb3973
Jason Garber <jason@dds.mil>
2018-07-18 19:14:00 +0000

    Fix up miscellanous roles issues

--- roles/zsh/files/.oh-my-zsh/custom/npm.zsh
+++ roles/zsh/files/.oh-my-zsh/custom/npm.zsh
@@ -1,2 +1,6 @@
+export PATH="$HOME/.node_modules/bin:$PATH"
+
 export NPM_CONFIG_DEPTH=0
+export NPM_CONFIG_PREFIX="$HOME/.node_modules"
 export NPM_CONFIG_PROGRESS=false
+export PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true
