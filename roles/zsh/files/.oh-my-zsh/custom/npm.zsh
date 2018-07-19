0abc80f90f43a786d6b62738a354b1e101ff141b
Jason Garber <jason@dds.mil>
2018-07-19 18:56:18 +0000

    Remove puppeteer env variable

--- roles/zsh/files/.oh-my-zsh/custom/npm.zsh
+++ roles/zsh/files/.oh-my-zsh/custom/npm.zsh
@@ -3,4 +3,3 @@ export PATH="$HOME/.node_modules/bin:$PATH"
 export NPM_CONFIG_DEPTH=0
 export NPM_CONFIG_PREFIX="$HOME/.node_modules"
 export NPM_CONFIG_PROGRESS=false
-export PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true
